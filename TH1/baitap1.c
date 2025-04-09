d9870d48434d5e15f6084ba47ef9a8530243acfc
thson0404 <107702116+thson0404@users.noreply.github.com>
2025-04-09 23:10:36 +0700

    Update baitap1.c

--- TH1/baitap1.c
+++ TH1/baitap1.c
@@ -4,5 +4,6 @@ int main() {
     int x; 
     printf("Nhap vao mot so: ");
     scanf("%d", &x);
-    printf("Binh phuong cua %d lï¿½: %d\n", x, x*x);
+    printf("Binh phuong cua %d là: %d\n", x, x*x);
     return 0;
+}
